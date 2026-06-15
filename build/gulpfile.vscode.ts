66326538bfa5ba4d415542cf6605ee26cf756193
zijianqiu <zijianqiu@tencent.com>
2026-06-15 06:59:19 +0000

    fix: exclude copilot prebuilds from rcedit (cross-platform .node files)

--- build/gulpfile.vscode.ts
+++ build/gulpfile.vscode.ts
@@ -587,7 +587,7 @@ function patchWin32DependenciesTask(destinationFolderName: string) {
 	return async () => {
 		const versionedResourcesFolder = util.getVersionedResourcesFolder('win32', commit!);
 		const deps = (await Promise.all([
-			glob('**/*.node', { cwd, ignore: 'extensions/node_modules/@parcel/watcher/**' }),
+			glob('**/*.node', { cwd, ignore: ['extensions/node_modules/@parcel/watcher/**', '**/copilot/**/prebuilds/**'] }),
 			glob('**/rg.exe', { cwd }),
 			glob('**/*explorer_command*.dll', { cwd }),
 		])).flatMap(o => o);
